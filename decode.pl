26fd970c56615a2c19cc9ca823e0d3876ac89acf
Andreas Heiduk <asheiduk@gmail.com>
2020-11-14 09:17:22 +0100

    reorder method into low-, mid- andhighlevel parsing

--- decode.pl
+++ decode.pl
@@ -10,6 +10,8 @@ use Carp;
 
 
 ################################################################
+#
+# low level parsing like primitves
 
 sub read_u8(*){
 	my $fh = shift;
@@ -100,6 +102,36 @@ sub read_count(*){
 }
 
 ################################################################
+#
+# mid level parsing
+
+# maybe helpfull: https://wiki.factorio.com/Version_string_format
+sub read_version(*){
+	my $fh = shift;
+	my ($main, $major, $minor, $developer) = (read_u16($fh), read_u16($fh), read_u16($fh),read_u16($fh));
+	printf "version: %d.%d.%d.%d\n", $main, $major, $minor, $developer;
+	return {
+		main => $main,
+		major => $major,
+		minor => $minor,
+		developer => $developer
+	};
+}
+
+sub read_migrations(*){
+	my $fh = shift;
+	my $result = [];
+	
+	my $count = read_count($fh);
+	printf "migrations: %d\n", $count;
+	for(my $i=0; $i<$count; ++$i){
+		my $mod_name = read_string($fh);
+		my $migration_file = read_string($fh);
+		printf "    [%d] mod '%s', migration '%s'\n", $i, $mod_name, $migration_file;
+		push @$result, { mod_name => $mod_name, migration_file => $migration_file }
+	}
+	return $result;
+}
 
 # maybe helpfull: https://wiki.factorio.com/Data_types
 # maybe helpfull: https://wiki.factorio.com/Types/Position
@@ -318,85 +350,8 @@ sub read_entity(*$$$){
 }
 
 ################################################################
-
-# maybe helpfull: https://wiki.factorio.com/Version_string_format
-sub read_version(*){
-	my $fh = shift;
-	my ($main, $major, $minor, $developer) = (read_u16($fh), read_u16($fh), read_u16($fh),read_u16($fh));
-	printf "version: %d.%d.%d.%d\n", $main, $major, $minor, $developer;
-	return {
-		main => $main,
-		major => $major,
-		minor => $minor,
-		developer => $developer
-	};
-}
-
-sub read_migrations(*){
-	my $fh = shift;
-	my $result = [];
-	
-	my $count = read_count($fh);
-	printf "migrations: %d\n", $count;
-	for(my $i=0; $i<$count; ++$i){
-		my $mod_name = read_string($fh);
-		my $migration_file = read_string($fh);
-		printf "    [%d] mod '%s', migration '%s'\n", $i, $mod_name, $migration_file;
-		push @$result, { mod_name => $mod_name, migration_file => $migration_file }
-	}
-	return $result;
-}
-
-sub read_types(*){
-	my $fh = shift;
-	my $result = {};
-	
-	my $cat_count = read_u16($fh);
-	printf "categories: %d\n", $cat_count;
-	for(my $c=0; $c<$cat_count; ++$c){
-	
-		my $cat_name = read_string($fh);
-		my $entry_count = read_count($fh);
-		
-		if( $cat_name eq "tile" ){		# TODO: strange exception
-			printf "    [%d] category '%s' - entries: %d\n", $c, $cat_name, $entry_count;
-			for(my $e=0; $e<$entry_count; ++$e){
-				my $entry_id = read_u8($fh);
-				my $entry_name = read_string($fh);
-				printf "        [%d] %02x '%s'\n", $e, $entry_id, $entry_name;
-				$result->{$cat_name."/".$entry_name} = $entry_id;
-			}
-
-			# 000001a0                 04 74 69  6c 65 07 02 08 63 6f 6e  |     .tile...con|
-			# 000001b0  63 72 65 74 65 03 14 68  61 7a 61 72 64 2d 63 6f  |crete..hazard-co|
-			# 000001c0  6e 63 72 65 74 65 2d 6c  65 66 74 04 15 68 61 7a  |ncrete-left..haz|
-			# 000001d0  61 72 64 2d 63 6f 6e 63  72 65 74 65 2d 72 69 67  |ard-concrete-rig|
-			# 000001e0  68 74 05 10 72 65 66 69  6e 65 64 2d 63 6f 6e 63  |ht..refined-conc|
-			# 000001f0  72 65 74 65 06 1c 72 65  66 69 6e 65 64 2d 68 61  |rete..refined-ha|
-			# 00000200  7a 61 72 64 2d 63 6f 6e  63 72 65 74 65 2d 6c 65  |zard-concrete-le|
-			# 00000210  66 74 07 1d 72 65 66 69  6e 65 64 2d 68 61 7a 61  |ft..refined-haza|
-			# 00000220  72 64 2d 63 6f 6e 63 72  65 74 65 2d 72 69 67 68  |rd-concrete-righ|
-			# 00000230  74 08 08 6c 61 6e 64 66  69 6c 6c                 |t..landfill     |
-
-			# tokens: "tile", 07, 02, "concrete", 03, "hazard-concrete-left", 04 "hazard-concrete-right",
-			# 	05, "refined-concrete", 06, "refined-hazard-concrete-left", 07, "refined-hazard-concrete-right",
-			# 	08, "landfill"
-			
-		}
-		else {
-			printf "    [%d] category '%s' - entries: %d\n", $c, $cat_name, $entry_count;
-			read_unknown($fh);
-			for(my $e=0; $e<$entry_count; ++$e){
-				# So far only "container/wooden chest" (0x0101) really needs two bytes.
-				my $entry_id = read_u16($fh);
-				my $entry_name = read_string($fh);
-				printf "        [%d] %04x '%s'\n", $e, $entry_id, $entry_name;
-				$result->{$cat_name."/".$entry_name} = $entry_id;
-			}
-		}
-	}
-	return $result;
-}
+#
+# blueprint
 
 sub read_blueprint(*$){
 	my $fh = shift;
@@ -477,6 +432,45 @@ sub read_blueprint(*$){
 	return $result;
 }
 
+################################################################
+#
+# blueprint library
+
+sub read_types(*){
+	my $fh = shift;
+	my $result = {};
+	
+	my $cat_count = read_u16($fh);
+	printf "categories: %d\n", $cat_count;
+	for(my $c=0; $c<$cat_count; ++$c){
+	
+		my $cat_name = read_string($fh);
+		my $entry_count = read_count($fh);
+		
+		if( $cat_name eq "tile" ){		# TODO: strange exception
+			printf "    [%d] category '%s' - entries: %d\n", $c, $cat_name, $entry_count;
+			for(my $e=0; $e<$entry_count; ++$e){
+				my $entry_id = read_u8($fh);
+				my $entry_name = read_string($fh);
+				printf "        [%d] %02x '%s'\n", $e, $entry_id, $entry_name;
+				$result->{$cat_name."/".$entry_name} = $entry_id;
+			}
+		}
+		else {
+			printf "    [%d] category '%s' - entries: %d\n", $c, $cat_name, $entry_count;
+			read_unknown($fh);
+			for(my $e=0; $e<$entry_count; ++$e){
+				# So far only "container/wooden chest" (0x0101) really needs two bytes.
+				my $entry_id = read_u16($fh);
+				my $entry_name = read_string($fh);
+				printf "        [%d] %04x '%s'\n", $e, $entry_id, $entry_name;
+				$result->{$cat_name."/".$entry_name} = $entry_id;
+			}
+		}
+	}
+	return $result;
+}
+
 sub get_type_id($$){
 	my $library = shift;
 	my $key = shift;
@@ -544,6 +538,10 @@ sub read_blueprint_library(*){
 	
 	return $result;
 }
+
+################################################################
+#
+# main
 
 sub dump_trailing_data(*){
 	my $fh = shift;
